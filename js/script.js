a2809ed575a05901ec93274da42a4816d3634b61
Patrick M <patrickymacdonald@gmail.com>
2022-10-27 01:06:27 -0400

    3

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function toggle0() {
+    var htmlShow = document.getElementById("content0");
+    if (htmlShow.style.display === "none") {
+        htmlShow.style.display = "block";
+    } else {
+        htmlShow.style.display = "none";
+    }
+}
+
+function toggle1() {
+    var htmlShow = document.getElementById("content1");
+    if (htmlShow.style.display === "none") {
+        htmlShow.style.display = "block";
+    } else {
+        htmlShow.style.display = "none";
+    }
+}
+
+function toggle2() {
+    var htmlShow = document.getElementById("content2");
+    if (htmlShow.style.display === "none") {
+        htmlShow.style.display = "block";
+    } else {
+        htmlShow.style.display = "none";
+    }
+}
+
+function toggle3() {
+    var htmlShow = document.getElementById("content3");
+    if (htmlShow.style.display === "none") {
+        htmlShow.style.display = "block";
+    } else {
+        htmlShow.style.display = "none";
+    }
+}
+
+function toggle4() {
+    var htmlShow = document.getElementById("content4");
+    if (htmlShow.style.display === "none") {
+        htmlShow.style.display = "block";
+    } else {
+        htmlShow.style.display = "none";
+    }
+}
